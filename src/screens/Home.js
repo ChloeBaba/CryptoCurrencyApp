a114a42489657759d11da81c0931964754a650e3
Chloebaba <chloe@neosilver.fr>
2021-03-22 20:16:18 +0100

    styling homepage and tracker page

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,21 +1,47 @@
 import React from 'react';
-import { SafeAreaView, Text, View, Button } from 'react-native';
+import { Button, Text, View, StyleSheet, SafeAreaView } from 'react-native';
 import CryptoCurList from '../components/CryptoCurList';
 
+// Home page Screen in which the general Crypto Currency list component is called
 export default function Home({ navigation }) {
 
-    const PressHandler = () => {
-        navigation.navigate('Tracker')
-      }
-
-    return (
-        <SafeAreaView>
-            <View>
-                <Button title='My Tracker >' onPress={PressHandler} />
-            </View>
-            <View>
-                <CryptoCurList />
-            </View>
+  const PressHandler = () => {
+    navigation.navigate('Tracker')
+  }
+  return (
+        <SafeAreaView style={styles.container}>
+                <View>
+                  <Button title='My Tracker >' onPress={PressHandler} />
+                </View>
+                <View style={styles.title}>
+                  <Text style={styles.textTitle}> Crypto Currency | Price USD | Price change (last 24h) </Text>
+                </View>
+                <Text>
+                    <View style={styles.contentList}>
+                        <CryptoCurList />
+                    </View>
+                </Text>
         </SafeAreaView>
-    );
+        );
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor:'white'
+  },
+  contentList: {
+    justifyContent:'center',
+    alignItems:'center',
+    padding: 24,
+    marginLeft:30,
+    backgroundColor:'white'
+    },
+    title: {
+      padding: 10,
+      marginLeft: 30
+    },
+    textTitle: {
+      fontWeight:'bold'
+    }
+  });
--- src/screens/Tracker.js
+++ src/screens/Tracker.js
@@ -1,11 +1,42 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
 import CryptoCurTracker from '../components/CryptoCurTracker';
 
+// Tracker page Screen in which the added/selected Crypto Currencies list component is called
 export default function Tracker() {
-    return (
-        <View>
-            <CryptoCurTracker />
+
+  return (
+      <SafeAreaView style={styles.container}>
+        <View style={styles.title}>
+          <Text style={styles.textTitle}> Crypto Currency | Price USD | Price change (last 24h) </Text>
         </View>
-    );
-}
+        <Text>
+            <View style={styles.content}>
+                <CryptoCurTracker />
+            </View>
+        </Text>
+    </SafeAreaView>
+  );
+        }
+        
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor:'white'
+  },
+  content: {
+    justifyContent:'center',
+    alignItems:'center',
+    padding: 24,
+    marginTop: 50,
+    marginLeft:30,
+    backgroundColor:'white'
+    },
+    title: {
+      padding: 10,
+      marginLeft: 30
+    },
+    textTitle: {
+      fontWeight:'bold'
+    }
+  });
