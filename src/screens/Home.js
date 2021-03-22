9a7c726cf550c584290ac7f897cfe5b2d43f50c4
Chloebaba <chloe@neosilver.fr>
2021-03-22 19:42:03 +0100

    fix navigationfrom home to tracker page + add navigation button to tracker page

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { SafeAreaView, Text, View, Button } from 'react-native';
 
-export default function Home() {
+export default function Home({ navigation }) {
+
+    const PressHandler = () => {
+        navigation.navigate('Tracker')
+      }
+
     return (
-        <View>
-            <Text>Home page</Text>
-        </View>
+        <SafeAreaView>
+            <View>
+                <Button title='My Tracker >' onPress={PressHandler} />
+            </View>
+            <View>
+                <Text>Home page</Text>
+            </View>
+        </SafeAreaView>
     );
 }
